94c1ec6c57ab1f0a28d4c44fbe613703f2938de4
DiogoWebber <138811393+DiogoWebber@users.noreply.github.com>
2024-05-02 22:48:49 -0300

    novo

--- controllers/userController.js
+++ controllers/userController.js
@@ -41,7 +41,7 @@ function createUser(req, res) {
         return res.send('Usuário já existe! Escolha outro nome de usuário.');
     }
     const id = uuidv4();
-    users.push({ id, username, password, email, level: 'public', status });
+    users.push({ id, username, password, email, level: 'off', status });
     saveUsers(users);
     return res.redirect('/html/user-created.html');
 }
